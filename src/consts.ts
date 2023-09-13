6ba56da7399e2ef4f0629246185e532ace6e5e3f
Noisk8 <juansk8jaramillo@gmail.com>
2023-09-12 19:14:11 -0500

    0.1

---
+++ src/consts.ts
@@ -0,0 +1,7 @@
+export const SITE_NAME = "latNYMamerica";
+export const SITE_TITLE = "latNYMamerica | A personal blog by Abdullah Elsayed (@abdllahdev)";
+export const SITE_DESCRIPTION =
+  "latNYMamerica | A personal blog by Abdullah Elsayed (@abdllahdev)";
+export const TWITTER_HANDLE = "https://twitter.com/abdllah_dev";
+export const GITHUB_HANDLE = "https://github.com/abdllahdev";
+export const CLOUDINARY_NAME = "example";
