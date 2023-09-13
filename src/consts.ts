db4b8b51b2f4b770afc49af31730ad0d8b5e1ab2
Noisk8 <juansk8jaramillo@gmail.com>
2023-09-12 19:38:06 -0500

    0.1.0

--- src/consts.ts
+++ src/consts.ts
@@ -1,7 +1,6 @@
 export const SITE_NAME = "latNYMamerica";
-export const SITE_TITLE = "latNYMamerica | A personal blog by Abdullah Elsayed (@abdllahdev)";
-export const SITE_DESCRIPTION =
-  "latNYMamerica | A personal blog by Abdullah Elsayed (@abdllahdev)";
-export const TWITTER_HANDLE = "https://twitter.com/abdllah_dev";
-export const GITHUB_HANDLE = "https://github.com/abdllahdev";
+export const SITE_TITLE = "latNYMamerica ";
+export const SITE_DESCRIPTION = "latNYMamerica | Operadorxs de nodos de nym en latinoamerica";
+export const TWITTER_HANDLE = "https://twitter.com/EspanolNym";
+export const GITHUB_HANDLE = "https://github.com/NymLatam/web";
 export const CLOUDINARY_NAME = "example";
