10d6111cdf23530bf045646a3f09a6c93d20311b
Noisk8 <juansk8jaramillo@gmail.com>
2023-09-12 22:02:36 -0500

    0.1.5

--- src/consts.ts
+++ src/consts.ts
@@ -3,4 +3,4 @@ export const SITE_TITLE = "latNYMamerica ";
 export const SITE_DESCRIPTION = "latNYMamerica | Operadorxs de nodos de nym en latinoamerica";
 export const TWITTER_HANDLE = "https://twitter.com/EspanolNym";
 export const GITHUB_HANDLE = "https://github.com/NymLatam/web";
-export const CLOUDINARY_NAME = "example";
+export const CLOUDINARY_NAME = "https://t.me/NYMSPANISH";
